2e2a9073ec0c8ce0c1491c53d1b75ceab0729593
Rebeca <rebeca99rs@gmail.com>
2022-04-06 13:27:18 -0300

    alterações

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,9 @@
-import {useState} from 'react';
-
 function Home() {
-    return ( <div>
-        <h6>Home</h6>
-        <Contador />
+    return  (<div>
+        <h6>Home</h6>       
+        <a href="/sobre">Acesse a página Sobre</a>
     </div>
     )
     }
 
-function Contador() {
-    const [contador,setContador] = useState(1)
-
-    function adicionarContador() {
-        setContador(contador +1);
-    }
-    return (
-        <div>
-            <div>{contador}
-            </div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,9 @@
+function Sobre() {
+    return ( <div>
+        <h6>Home</h6>       
+        <a href="/">Acesse a página Home</a>
+    </div>
+    )
+    }
+
+export default Home
